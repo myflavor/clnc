78fabe022678ccbd58cf773e16fa4d0cd7183fe4
MyFlavor <45022747+myflavor@users.noreply.github.com>
2021-08-03 13:30:27 +0800

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,15 @@
+apk add proxychains-ng
+sed -i '$d' /etc/proxychains/proxychains.conf
+sed -i '$s/.*/http 127.0.0.1 6650/' /etc/proxychains/proxychains.conf
+wget https://cdn.jsdelivr.net/gh/myflavor/clnc@master/proxy.zip
+rm -rf /etc/clnc
+mkdir /etc/clnc
+unzip proxy.zip -d /etc/clnc
+rm -rf proxy.zip
+chmod +x /etc/clnc/clnc
+rm -rf /usr/local/bin/clnc
+echo "/etc/clnc/clnc -c /etc/clnc/clnc.conf" >> /usr/local/bin/clnc
+chmod +x /usr/local/bin/clnc
+sed -i 's/^EnableExtraShell=.*/EnableExtraShell="true"/g' /jd/config/config.sh
+echo "sed -i 's/node /proxychains -q node /g' jd.sh" >> /jd/config/diy.sh
+clnc
